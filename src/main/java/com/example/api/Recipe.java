968c706a850e59159a8cda89c714f7c0a040a62a
hirotaka.yokoyama <hirotaka.yokoyama@rakus-partners.co.jp>
2026-04-27 14:00:32 +0900

    use render db env

--- src/main/java/com/example/api/Recipe.java
+++ src/main/java/com/example/api/Recipe.java
@@ -1,6 +1,8 @@
 package com.example.api;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -9,6 +11,7 @@ import jakarta.persistence.Table;
 public class Recipe {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
@@ -16,43 +19,53 @@ public class Recipe {
     private String serves;
     private String ingredients;
     private int cost;
-    
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getMaking_time() {
         return making_time;
     }
+
     public void setMaking_time(String making_time) {
         this.making_time = making_time;
     }
+
     public String getServes() {
         return serves;
     }
+
     public void setServes(String serves) {
         this.serves = serves;
     }
+
     public String getIngredients() {
         return ingredients;
     }
+
     public void setIngredients(String ingredients) {
         this.ingredients = ingredients;
     }
+
     public int getCost() {
         return cost;
     }
+
     public void setCost(int cost) {
         this.cost = cost;
     }
 
-    
 }
